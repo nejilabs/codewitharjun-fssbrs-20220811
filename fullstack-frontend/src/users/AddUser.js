f7a9859a3f483c9905a2c47cdb3c04168d9ef2c8
根島憲紀-魏新栩 <nelsonalbajr@yahoo.com>
2022-10-22 16:14:08 +0900

    feat/nelly/13suiis-14 @e

--- fullstack-frontend/src/users/AddUser.js
+++ fullstack-frontend/src/users/AddUser.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function AddUser() {
+
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: ""
+  })
+
+  const { name, username, email } = user;
+
+  const onInputChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
+
+
   return (
     <div className='container'>
       <div className="row">
@@ -8,13 +22,13 @@ export default function AddUser() {
           <h2 className="text-center m-4">Register User</h2>
           <div className="mb-3">
             <label htmlFor="Name" className="form-label">Name</label>
-            <input type={"text"} className="form-control" placeholder="Enter your name" name="name" />
+            <input type={"text"} className="form-control" placeholder="Enter your name" name="name" value={name} onChange={(e) => onInputChange(e)} />
 
             <label htmlFor="Username" className="form-label">Username</label>
-            <input type={"text"} className="form-control" placeholder="Enter your username" name="username" />
+            <input type={"text"} className="form-control" placeholder="Enter your username" name="username" value={username} onChange={(e) => onInputChange(e)} />
 
             <label htmlFor="E-mail" className="form-label">E-mail</label>
-            <input type={"text"} className="form-control" placeholder="Enter your e-mail address" name="email" />
+            <input type={"text"} className="form-control" placeholder="Enter your e-mail address" name="email" value={email} onChange={(e) => onInputChange(e)} />
           </div>
           <button type="submit" className="btn btn-outline-primary">Submit</button>
           <button type="submit" className="btn btn-outline-danger mx-2">Cancel</button>
